3d826fe2ea2016ce8911eb451d21801f144f8c6c
linhdan2109 <85982220+linhdan2109@users.noreply.github.com>
2022-05-21 22:34:03 +0700

    Update doan code R.r

--- doan code R.r
+++ doan code R.r
@@ -1,48 +1,51 @@
-#B3 xac dinh dang ham hoi quy
-#bwght = b0 + b1faminc + b2cigs + b3male + b4cigtax + b5cigprice+ u # nolint
+# B3: Xác định dạng hàm hồi quy
+# bwght = b0 + b1faminc + b2cigs + b3male + b4cigtax + b5cigprice+ u # nolint
 
-#B4: tien hanh hoi quy
+# B4: tiến hành hồi quy
 library(wooldridge)
 kqua <- lm(bwght ~ faminc + male + cigs + cigtax + cigprice, data = bwght)
 summary(kqua)
 
-#B5 kiem dinh sai dang ham (kiem dinh RESET)
+# B5 kiểm định sai dạng hàm (kiểm định RESET)
 library(lmtest)
 resettest(lm(bwght ~ faminc + male + cigs + cigtax + cigprice, data = bwght))
 
-#B6 kiem dinh pp chuan cua u (neu mau lon ta co the suy ra mau tiem can chuan)
+# B6 Kiểm định phân phối chuẩn của u (Kiểm định giả thuyết MLR. 6)
+# cách 1
 library(normtest)
 u <- resid(kqua)
+# cách 2
 jb.norm.test(u)
 shapiro.test(u)
 
-#B7 kiem dinh phuong sai thay doi
+# B7 Kiểm định phương sai thay đổi (Kiểm định giả thuyết MLR. 5)
+# bp test
 library(lmtest)
 bptest(kqua)
 #white test
 bptest(kqua, ~ fitted(kqua) + I(fitted(kqua)^2))
 
-#B8.1 kiem dinh cac bien co y nghia thong ke hay khong (kiem dinh t)
+# B8a kiểm định ý nghĩa riêng lẻ các biến (kiểm định t)
 summary(kqua)
 
-#B8.2 kiem dinh dong thoi
-# kiem dinh F
+# B8b kiểm định đồng thời
+# kiểm định F
 library(car)
 h0 <- c("cigtax=0", "cigprice=0")
 linearHypothesis(kqua, h0)
 
-# kiem dinh Lagrange
-# 1. mo hinh rang buoc
+# kiểm định nhân tử Lagrange
+# 1. Hồi quy y theo mô hình đã gán ràng buộc
 kqua_restr <- lm(bwght ~ faminc + cigs + male, data = bwght)
-# 2. tim phan du cua mo hinh rang buoc
+# 2. Tìm phần dư u~ của mô hình đã gán ràng buộc
 u_restr <- resid(kqua_restr)
-# 3. hoi quy tat ca cac bien theo phan du cua mo hinh rang buoc
+# 3. Hồi quy u~ theo tất cả các biến độc lập. Từ đó tìm được Ru~^2
 lagrange <- lm(u_restr ~ faminc + cigs + male + cigtax + cigprice, data = bwght)
-# 4. tinh LM = r^2 * n
+# 4. tính LM = Ru~^2 * n
 lm <- summary(lagrange)$r.squared * nobs(lagrange)
-# tính p-value. Bac bo H0 neu p-value < a
+# Tính p-value
 1 - pchisq(lm, 2)
 
-#9 mo hinh hoi quy dung
+# B9 mô hình hồi quy đúng
 kqua_dung <- lm(bwght ~ faminc + male + cigs, data = bwght)
 summary(kqua_dung)

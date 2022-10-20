db0cbf7c3691ca696353f57690ecc88a079582a7
Linh Dan Pham <85982220+linhdan2109@users.noreply.github.com>
2022-10-21 01:10:45 +0700

    Update SQL_ProcessPharse.sql

--- Google Data Analytics Professional Certificate Capstone Project/SQL_ProcessPharse.sql
+++ Google Data Analytics Professional Certificate Capstone Project/SQL_ProcessPharse.sql
@@ -1,4 +1,3 @@
-
 --Show table schema
 SELECT 
 	COLUMN_NAME,
@@ -12,9 +11,11 @@ WHERE TABLE_NAME = 'dailyActivity_merged'
 	OR TABLE_NAME = 'sleepDay_merged'
 	OR TABLE_NAME = 'weightLogInfo_merged'
 
+
 -- Show a number of rows
 SELECT TOP (1000) *
 FROM [Bellabeat].[dbo].[dailyActivity_merged]
+
 
 -- Size of the table
 SELECT COUNT(*) AS dailyActivity
@@ -35,6 +36,7 @@ FROM [Bellabeat].[dbo].[sleepDay_merged]
 SELECT COUNT(*) AS weightLogInfo
 FROM [Bellabeat].[dbo].[weightLogInfo_merged]
 
+
 -- Check if the date in the dateset fit in the range 3/12/2016 - 5/12/2016
 SELECT DISTINCT ActivityDate 
 FROM [Bellabeat].[dbo].[dailyActivity_merged]
@@ -47,6 +49,7 @@ WHERE NOT(SleepDay BETWEEN '2016-03-12' AND '2016-05-12')
 SELECT DISTINCT Date 
 FROM [Bellabeat].[dbo].[weightLogInfo_merged]
 WHERE NOT(Date BETWEEN '2016-03-12' AND '2016-05-12')
+
 
 -- Check to see if we have 30 users
 SELECT COUNT(DISTINCT Id) AS dailyActivity
@@ -67,6 +70,7 @@ SELECT
 FROM [Bellabeat].[dbo].[dailyActivity_merged]
 GROUP BY Id, ActivityDate
 HAVING COUNT(*) >= 2
+
 
 -- Add a column for day of the week
 SELECT TOP (1000) 
